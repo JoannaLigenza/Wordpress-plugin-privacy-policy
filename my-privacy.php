31eacfc9c5f45fbc8bc9bbcbeb4460b05421df1e
JoannaLigenza <ligenzajoanna@gmail.com>
2020-02-13 21:38:14 +0100

    dispplay privacy policy button depends on setting on Privacy Policy Page

--- my-privacy.php
+++ my-privacy.php
@@ -85,18 +85,19 @@ function jlplg_prvpol_add_new_page() {
 function jlplg_prvpol_page_html_content() {
     ?>
     <div class="wrap">
-      <form action="options.php" method="post">
-        <h2>Page title</h2>
-        <?php
-        // outpus settings fields (without this there is error after clicking save settings button)
-        settings_fields( 'jl_options' );                        // A settings group name. This should match the group name used in register_setting()
-        // output setting sections and their fields
-        do_settings_sections( 'jl-slug' );                      // The slug name of settings sections you want to output.
-        // output save settings button
-        $other_attributes = array( 'style' => 'background-color: white; color: blue; border: 1px solid blue' );
-        submit_button( 'Save Settings', 'primary', 'submit', true, $other_attributes );     // Button text, button type, button id, wrap, any other attribute
-        ?>
-      </form>
+        <h2>Privacy Policy & Cookie Info</h2>
+        <form action="options.php" method="post">
+            
+            <?php
+            // outpus settings fields (without this there is error after clicking save settings button)
+            settings_fields( 'jl_options' );                        // A settings group name. This should match the group name used in register_setting()
+            // output setting sections and their fields
+            do_settings_sections( 'jl-slug' );                      // The slug name of settings sections you want to output.
+            // output save settings button
+            $other_attributes = array( 'style' => 'background-color: white; color: blue; border: 1px solid blue' );
+            submit_button( 'Save Settings', 'primary', 'submit', true, $other_attributes );     // Button text, button type, button id, wrap, any other attribute
+            ?>
+        </form>
     </div>
     <?php
 }
@@ -104,19 +105,26 @@ function jlplg_prvpol_page_html_content() {
 // adding settings and sections
 function jlplg_prvpol_add_new_settings() {
     // register setting
-    $settins_arg = array(
+    $settins_field1_arg = array(
         'type' => 'string',
         'description' => 'descrption of settings for field1 and field2',
         'sanitize_callback' => 'jlplg_prvpol_sanitize_input_field',
-        'default' => 'now'
+        'default' => 'We use cookies to improve your experience on our website. By browsing this website, you agree to our use of cookies'
     );
-    register_setting( 'jl_options', 'field1', $settins_arg);     // option group, option name, args
-    register_setting( 'jl_options', 'field2', $settins_arg);
+    $settins_field2_arg = array(
+        'type' => 'boolean',
+        'sanitize_callback' => 'jlplg_prvpol_sanitize_checkbox',
+        'default' => true
+    );
+    register_setting( 'jl_options', 'jlplg_prvpol-field1-cookie-message', $settins_field1_arg);     // option group, option name, args
+    register_setting( 'jl_options', 'jlplg_prvpol-field2-checkbox-privacy-policy', $settins_field2_arg);     // option group, option name, args
+    register_setting( 'jl_options', 'field2', $settins_field1_arg);
     // adding sections
-    add_settings_section( 'jl_setting_section_1', 'Section Title 1 - adding from section 1 options', 'jlplg_prvpol_section_1_text', 'jl-slug' );  // id (Slug-name to identify the section), title, callback, page slug
+    add_settings_section( 'jlplg_prvpol_section_1_configuration', 'Configuration', null, 'jl-slug' );  // id (Slug-name to identify the section), title, callback, page slug
     add_settings_section( 'jl_setting_section_2', 'Section Title 2 - adding from section 2 options', 'jlplg_prvpol_section_2_text', 'jl-slug' );
     // adding fields for section
-    add_settings_field( 'field-1', 'Field 1', 'jlplg_prvpol_field_callback1', 'jl-slug', 'jl_setting_section_1' );       // id (Slug-name to identify the field), title, callback, slug-name of the settings page on which to show the section, section, args (attr for field)
+    add_settings_field( 'field-1-cookie-message', 'Cookie Message', 'jlplg_prvpol_field_1_callback', 'jl-slug', 'jlplg_prvpol_section_1_configuration' );       // id (Slug-name to identify the field), title, callback, slug-name of the settings page on which to show the section, section, args (attr for field)
+    add_settings_field( 'field-2-privacy-policy-button', 'Display Privacy Policy Button', 'jlplg_prvpol_field_2_callback', 'jl-slug', 'jlplg_prvpol_section_1_configuration' );       // id (Slug-name to identify the field), title, callback, slug-name of the settings page on which to show the section, section, args (attr for field)
     add_settings_field( 'field-2', 'Field 2', 'jlplg_prvpol_field_callback2', 'jl-slug', 'jl_setting_section_2' );
 }
 add_action( 'admin_init', 'jlplg_prvpol_add_new_settings' );
@@ -130,19 +138,35 @@ function jlplg_prvpol_section_2_text() {
     echo 'to jest tresc sekcji 2';
 }
 
-function jlplg_prvpol_field_callback1() {
-  echo '<input type="text" name="field1" value="'.get_option( "field1" ).'" />';
+function jlplg_prvpol_field_1_callback() {
+    echo '<textarea type="text" cols="50" rows="4" name="jlplg_prvpol-field1-cookie-message" >'.get_option( "jlplg_prvpol-field1-cookie-message" ).'</textarea>';
+}
+
+function jlplg_prvpol_field_2_callback() {
+    if ( get_option( "jlplg_prvpol-field2-checkbox-privacy-policy" ) ) {
+        echo '<input type="checkbox" name="jlplg_prvpol-field2-checkbox-privacy-policy" checked />';
+    } else {
+        echo '<input type="checkbox" name="jlplg_prvpol-field2-checkbox-privacy-policy" />';
+    }
 }
 
 function jlplg_prvpol_field_callback2() {
-  echo '<input type="text" name="field2" value="" />';
+    echo '<textarea type="text" cols="50" rows="4" name="jlplg_prvpol_field1_message" value="'.get_option( "jlplg_prvpol_message" ).'" >'.get_option( "field2" ).'</textarea>';
+
+    echo '<input type="text" name="field2" value="'.get_option( "field2" ).'" />';
 }
 
+// sanitize input
 function jlplg_prvpol_sanitize_input_field( $input ) {
     if ( isset( $input ) ) {
         $input = sanitize_text_field( $input );
     }
     return $input;
+}
+
+// sanitize checkbox
+function jlplg_prvpol_sanitize_checkbox( $checked ) {
+    return ( ( isset( $checked ) && true == $checked ) ? true : false );
 }
 
 // set cookie
@@ -161,7 +185,9 @@ function jlplg_prvpol_your_function() {
         <form action="" method="post" id="cookie-form">
             <p class="cookie-info">This is inserted at the bottom This is inserted at the bottom</p>
             <button type="submit" name="cookie-accept-button" class="cookie-accept-button" id="cookie-accept-button">Accept Cookies</button>
-            <button type="submit" name="cookie-privacy-policy" class="cookie-privacy-policy" id="cookie-privacy-policy">Privacy policy</button>  
+            <?php if ( get_option( "jlplg_prvpol-field2-checkbox-privacy-policy" ) ) { ?>
+            <button type="submit" name="cookie-privacy-policy" class="cookie-privacy-policy" id="cookie-privacy-policy">Privacy policy</button>
+            <?php } ?>
         </form>
     </div>
 <?php
