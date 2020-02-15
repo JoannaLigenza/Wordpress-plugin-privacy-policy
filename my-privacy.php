1c3768cf49f64824ef8f5168d00d06b44b3ede52
JoannaLigenza <ligenzajoanna@gmail.com>
2020-02-15 18:13:33 +0100

    added posiibility to change cookie info placement

--- my-privacy.php
+++ my-privacy.php
@@ -85,8 +85,9 @@ function jlplg_prvpol_display_cookie_info() {
     $text_color = empty( get_option( "jlplg_prvpol-field5-text-color" ) ) ? '#ffffff' : get_option( "jlplg_prvpol-field5-text-color" );
     $button_background_color = empty( get_option( "jlplg_prvpol-field6-button-background-color" ) ) ? '#dcf1ff' : get_option( "jlplg_prvpol-field6-button-background-color" );
     $button_text_color = empty( get_option( "jlplg_prvpol-field7-button-text-color" ) ) ? '#000000' : get_option( "jlplg_prvpol-field7-button-text-color" );
+    $cookie_info_placemet = empty( get_option( "jlplg_prvpol-field4-cookie-plugin-placement" ) ) ? 'bottom' : get_option( "jlplg_prvpol-field4-cookie-plugin-placement" );
 ?>
-    <div class="jlplg-prvpol-cookie-info-container" style="background-color: <?php echo $background_color; ?>">
+    <div class="jlplg-prvpol-cookie-info-container" style="background-color: <?php echo $background_color.'; '.$cookie_info_placemet.': 0' ?>">
         <form action="" method="post" id="cookie-form">
             <p class="jlplg-prvpol-cookie-info" style="color: <?php echo $text_color ?>"><?php echo $cookie_message; ?></p>
             <div class="jlplg-prvpol-buttons">
@@ -163,6 +164,11 @@ function jlplg_prvpol_add_new_settings() {
         'sanitize_callback' => 'jlplg_prvpol_sanitize_input_field',
         'default' => 'Accept Cookies'
     );
+    $configuration_settins_field4_arg = array(
+        'type' => 'string',
+        'sanitize_callback' => 'jlplg_prvpol_sanitize_input_field',
+        'default' => 'bottom'
+    );
     $layout_settins_field1_arg = array(
         'type' => 'string',
         'sanitize_callback' => 'jlplg_prvpol_sanitize_input_field',
@@ -186,6 +192,7 @@ function jlplg_prvpol_add_new_settings() {
     register_setting( 'jl_options', 'jlplg_prvpol-field1-cookie-message', $configuration_settins_field1_arg);     // option group, option name, args
     register_setting( 'jl_options', 'jlplg_prvpol-field2-checkbox-privacy-policy', $configuration_settins_field2_arg);
     register_setting( 'jl_options', 'jlplg_prvpol-field3-cookie-button-text', $configuration_settins_field3_arg);
+    register_setting( 'jl_options', 'jlplg_prvpol-field4-cookie-plugin-placement', $configuration_settins_field4_arg);
     register_setting( 'jl_options', 'jlplg_prvpol-field4-background-color', $layout_settins_field1_arg);
     register_setting( 'jl_options', 'jlplg_prvpol-field5-text-color', $layout_settins_field2_arg);
     register_setting( 'jl_options', 'jlplg_prvpol-field6-button-background-color', $layout_settins_field3_arg);
@@ -197,10 +204,11 @@ function jlplg_prvpol_add_new_settings() {
     add_settings_field( 'field-1-cookie-message', 'Cookie Message', 'jlplg_prvpol_field_1_callback', 'jl-slug', 'jlplg_prvpol_section_1_configuration' );       // id (Slug-name to identify the field), title, callback, slug-name of the settings page on which to show the section, section, args (attr for field)
     add_settings_field( 'field-2-privacy-policy-button', 'Display Privacy Policy Button', 'jlplg_prvpol_field_2_callback', 'jl-slug', 'jlplg_prvpol_section_1_configuration' );
     add_settings_field( 'field-3-cookie-button-text', 'Cookie Button Text', 'jlplg_prvpol_field_3_callback', 'jl-slug', 'jlplg_prvpol_section_1_configuration' );
-    add_settings_field( 'field-4-cookie-background-color', 'Background color', 'jlplg_prvpol_field_4_callback', 'jl-slug-2', 'jlplg_prvpol_section_2_layout' );
-    add_settings_field( 'field-5-cookie-text-color', 'Text color', 'jlplg_prvpol_field_5_callback', 'jl-slug-2', 'jlplg_prvpol_section_2_layout' );
-    add_settings_field( 'field-6-cookie-button-background-color', 'Button background color', 'jlplg_prvpol_field_6_callback', 'jl-slug-2', 'jlplg_prvpol_section_2_layout' );
-    add_settings_field( 'field-7-cookie-button-text-color', 'Button text color', 'jlplg_prvpol_field_7_callback', 'jl-slug-2', 'jlplg_prvpol_section_2_layout' );
+    add_settings_field( 'field-4-cookie-plugin-placement', 'Cookie info placement', 'jlplg_prvpol_field_4_callback', 'jl-slug', 'jlplg_prvpol_section_1_configuration' );
+    add_settings_field( 'field-5-cookie-background-color', 'Background color', 'jlplg_prvpol_field_5_callback', 'jl-slug-2', 'jlplg_prvpol_section_2_layout' );
+    add_settings_field( 'field-6-cookie-text-color', 'Text color', 'jlplg_prvpol_field_6_callback', 'jl-slug-2', 'jlplg_prvpol_section_2_layout' );
+    add_settings_field( 'field-7-cookie-button-background-color', 'Button background color', 'jlplg_prvpol_field_7_callback', 'jl-slug-2', 'jlplg_prvpol_section_2_layout' );
+    add_settings_field( 'field-8-cookie-button-text-color', 'Button text color', 'jlplg_prvpol_field_8_callback', 'jl-slug-2', 'jlplg_prvpol_section_2_layout' );
 }
 add_action( 'admin_init', 'jlplg_prvpol_add_new_settings' );
 
@@ -223,23 +231,32 @@ function jlplg_prvpol_field_3_callback() {
     echo '<input type="text" name="jlplg_prvpol-field3-cookie-button-text" value="'.get_option( "jlplg_prvpol-field3-cookie-button-text" ).'" />';
 }
 
-// field 4 - background color
+// field 4 - cookie info placement
 function jlplg_prvpol_field_4_callback() {
+    $isChecked = empty( get_option( "jlplg_prvpol-field4-cookie-plugin-placement" ) ) ? 'bottom' : get_option( "jlplg_prvpol-field4-cookie-plugin-placement" );
+    ?>
+    <input type="radio" name="jlplg_prvpol-field4-cookie-plugin-placement" value="top" <?php echo $isChecked === 'top' ? "checked" : null ?> /> Top <br><br>
+    <input type="radio" name="jlplg_prvpol-field4-cookie-plugin-placement" value="bottom" <?php echo $isChecked === 'bottom' ? "checked" : null ?> /> Bottom
+    <?php
+}
+
+// field 5 - background color
+function jlplg_prvpol_field_5_callback() {
     echo '<input type="color" name="jlplg_prvpol-field4-background-color" value="'.get_option( "jlplg_prvpol-field4-background-color" ).'" />';
 }
 
-// field 5 - text color
-function jlplg_prvpol_field_5_callback() {
+// field 6 - text color
+function jlplg_prvpol_field_6_callback() {
     echo '<input type="color" name="jlplg_prvpol-field5-text-color" value="'.get_option( "jlplg_prvpol-field5-text-color" ).'" />';
 }
 
-// field 6 - button background color
-function jlplg_prvpol_field_6_callback() {
+// field 7 - button background color
+function jlplg_prvpol_field_7_callback() {
     echo '<input type="color" name="jlplg_prvpol-field6-button-background-color" value="'.get_option( "jlplg_prvpol-field6-button-background-color" ).'" />';
 }
 
-// field 7 - button text color
-function jlplg_prvpol_field_7_callback() {
+// field 8 - button text color
+function jlplg_prvpol_field_8_callback() {
     echo '<input type="color" name="jlplg_prvpol-field7-button-text-color" value="'.get_option( "jlplg_prvpol-field7-button-text-color" ).'" />';
 }
 
